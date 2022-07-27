12ce34059a493bf01b313848ff388224a8cf2125
Frank Gaspar <frank.gasparfcytsis@gmail.com>
2022-07-27 10:58:01 -0400

    Create list of categories

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,8 +1,26 @@
+import { useState } from 'react';
+
 const GifExpertApp = () => {
+  // const categories = ['One Punch', 'Samurai X', 'Dragon Ball'];
+  const [categories, setCategories] = useState([
+    'One Punch',
+    'Samurai X',
+    'Dragon Ball',
+  ]);
+  const handleAdd = () => {
+    setCategories(['Spiderman', ...categories]);
+  };
   return (
     <div>
       <h2>GifExpertApp</h2>
       <hr />
+
+      <button onClick={handleAdd}>Agregar</button>
+      <ol>
+        {categories.map((category) => {
+          return <li key={category}>{category}</li>;
+        })}
+      </ol>
     </div>
   );
 };
