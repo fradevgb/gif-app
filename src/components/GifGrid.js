2d90d8400dfa9fe9444e9140611cbc99676e91e0
Frank Gaspar <frank.gasparfcytsis@gmail.com>
2022-07-28 14:43:12 -0400

    Add custom hook gif api

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,17 +1,15 @@
-import { useState, useEffect } from 'react';
-import getGifs from '../helpers/getGifs';
+import { useFetchGifs } from '../hooks/useFetchGifs';
 import GifGridItem from './GifGridItem';
 
 const GifGrid = ({ category }) => {
-  const [images, setImages] = useState([]);
-
-  useEffect(() => {
-    getGifs(category).then(setImages).catch(console.log);
-  }, [category]);
+  const { data: images, loading } = useFetchGifs(category);
 
   return (
     <>
       <h3>{category}</h3>
+
+      {loading && <p>Loading...</p>}
+
       <div className="images">
         {images.map((img) => (
           <GifGridItem key={img.id} {...img} />
